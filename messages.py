5fa47d9fb5ed1ec0cb65f373f610cfcd88bebefa
Kartik <kartiksingh45088@gmail.com>
2026-05-10 18:57:29 +0200

    messages types

--- messages.py
+++ messages.py
@@ -1,12 +1,19 @@
-from langchain_core.messages import SystemMessages, HumanMessages, AIMessage
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from langchain_openai import ChatOpenAI
 
+from dotenv import load_dotenv
+
 load_dotenv()
+
 model = ChatOpenAI(model='gpt-4o-mini')
 
 messages = [
-    SystemMessages(content="You are a helpful assistant."),
-    HumanMessages(content="What is the capital of France?"),
+    SystemMessage(content="You are a helpful assistant."),
+    HumanMessage(content="What is the capital of France?"),
 ]
 
-model.invoke(messages)
+result = model.invoke(messages)
+
+messages.append(AIMessage(content=result.content))
+
+print(messages)
