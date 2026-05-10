d2818ae9888edf8a4b066fbbc270dbf3842a24a4
Kartik <kartiksingh45088@gmail.com>
2026-05-10 18:55:52 +0200

    chatbot

--- chatbot.py
+++ chatbot.py
@@ -1,14 +1,23 @@
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from dotenv import load_dotenv
 
-laod_dotenv()
+load_dotenv()
 
 model = ChatOpenAI(model='gpt-4o-mini')
 
+chat_history = [
+    SystemMessage(content="You are a helpful assistant."),
+]
+
 while True:
     user_input = input('You: ')
-     if user_input == 'exit':
+    chat_history.append(HumanMessage(content=user_input))
+    if user_input == 'exit':
         break
-    result = model.invoke(user_input)
+    result = model.invoke(chat_history)
+    chat_history.append(AIMessage(content=result.content))
+
     print('Chatbot:', result.content)
-    
+
+print(chat_history)
