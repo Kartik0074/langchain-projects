a92d5b5f6d192fb313315e3c814180db536575a1
Kartik <kartik2368@outlook.com>
2026-05-11 23:45:28 +0200

    debugged

--- 5.struct/output_parser.py
+++ 5.struct/output_parser.py
@@ -18,9 +18,20 @@ template = PromptTemplate(
 )
 
 template = PromptTemplate(
-    template = 'write a 5 line summary report on {topic}',
-    input_variables=['topic']
+    template = 'write a 5 line summary report on {text}',
+    input_variables=['text']
 )
+
+prompt1 = template.invoke({'topic': 'black hole'})
+
+result = model.invoke(prompt1)  
+
+prompt2 = template.invoke({'text': result.content})
+
+
+ result1 =model.invoke(prompt2)
+
+print(result1.content )
 
 
 
